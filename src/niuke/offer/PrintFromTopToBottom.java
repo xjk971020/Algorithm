5a78c9307a619df7e3e2db7e9c253619119a97b8
Cathetine <36163574+xjk971020@users.noreply.github.com>
2019-06-14 09:27:51 +0800

    从上往下打印二叉树

---
+++ src/niuke/offer/PrintFromTopToBottom.java
@@ -0,0 +1,33 @@
+package niuke.offer;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+/**
+
+ * author:xjk
+ * 2019/6/14 15:32
+ * niuke.offer
+ * 从上往下打印出二叉树的每个节点，同层节点从左至右打印。
+ */
+public class PrintFromTopToBottom {
+    public ArrayList<Integer> printFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode temp = queue.poll();
+            result.add(temp.val);
+            if (temp.left != null) {
+                queue.add(temp.left);
+            }
+            if (temp.right != null) {
+                queue.add(temp.right);
+            }
+        }
+        return result;
+    }
+}
