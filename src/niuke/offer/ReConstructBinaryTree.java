146f9e7e480172c11ef81d537c39941855e11082
Cathetine <36163574+xjk971020@users.noreply.github.com>
2019-06-19 09:32:57 +0800

    重建二叉树

--- src/niuke/offer/ReConstructBinaryTree.java
+++ src/niuke/offer/ReConstructBinaryTree.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
  * 例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
  */
 public class ReConstructBinaryTree {
+    
     public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
         if (pre.length != in.length) {
             throw new RuntimeException("前序遍历数组长度与中序遍历数组长度不相等");
