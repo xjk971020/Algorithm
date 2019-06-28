b41ac705bb4aca992e849867ad6cd184d79389dc
xjk971020 <823840954@qq.com>
2019-06-28 13:37:51 +0800

    和为S的连续整数序列

--- src/niuke/offer/FindContinuousSequence.java
+++ src/niuke/offer/FindContinuousSequence.java
@@ -1,8 +1,11 @@
 package niuke.offer;
 
+import java.util.ArrayList;
+
 /**
  * @author xjk
  * @date 2019/6/26 -  10:33
+ * 和为S的连续正数序列
  * 题目描述
  * 小明很喜欢数学,有一天他在做数学作业时,要求计算出9~16的和,他马上就写出了正确答案是100。
  * 但是他并不满足于此,他在想究竟有多少种连续的正数序列的和为100(至少包括两个数)。
@@ -10,4 +13,40 @@ package niuke.offer;
  * 现在把问题交给你,你能不能也很快的找出所有和为S的连续正数序列? Good Luck!
  **/
 public class FindContinuousSequence {
+    public ArrayList<ArrayList<Integer>> findContinuousSequence(int sum) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if (sum < 0) {
+            throw new RuntimeException("sum不能小于0");
+        }
+        if (sum < 3) {
+            return res;
+        }
+        int small = 1;
+        int big = 2;
+        int curNum = big + small;
+        int middle = (1 + sum)/2;
+        while (small < middle) {
+            if (curNum == sum) {
+                addContinuousSequence(res, small, big);
+            }
+            while(small < middle && curNum > sum) {
+                curNum -= small;
+                small++;
+                if (curNum == sum) {
+                    addContinuousSequence(res, small, big);
+                }
+            }
+            big++;
+            curNum += big;
+        }
+        return res;
+    }
+
+    private void addContinuousSequence(ArrayList<ArrayList<Integer>> res, int small, int big) {
+        ArrayList<Integer> level = new ArrayList<>();
+        for (int i = small; i <= big; ++i) {
+            level.add(i);
+        }
+        res.add(level);
+    }
 }
