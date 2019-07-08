91e29b75179cf07bd85cf94a6c70aaac7ce0717a
Cathetine <36163574+xjk971020@users.noreply.github.com>
2019-07-08 11:02:55 +0800

    二进制中1的个数

---
+++ src/niuke/offer/NumberOf1.java
@@ -0,0 +1,23 @@
+package niuke.offer;
+
+/**
+ * author:xjk
+ * 2019/7/8 11:00
+ * niuke.offer
+ * 二进制中1的个数
+ * 题目描述
+ * 输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
+ */
+ public class NumberOf1{
+    public int NumberOf1(int n) {
+        int flag = 1;
+        int count = 0;
+        while (flag != 0) {
+            if ((n & flag) != 0){
+                count++;
+            }
+            flag = flag << 1;
+        }
+        return count;
+    }
+ }
