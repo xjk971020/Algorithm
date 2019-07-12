890be197923d9d4f832e47c5a809dcadf37d6758
xjk971020 <823840954@qq.com>
2019-07-12 11:47:41 +0800

    矩形覆盖

--- src/niuke/offer/RectCover.java
+++ src/niuke/offer/RectCover.java
@@ -4,6 +4,8 @@ package niuke.offer;
  * author:xjk
  * 2019/6/13 16:50
  * niuke.offer
+ * 矩形覆盖
+ * 题目描述
  * 我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
  */
 public class RectCover {
@@ -15,14 +17,14 @@ public class RectCover {
         }
     }
 
-    public int RectCover(int target) {
+    public int rectCover(int target) {
         if (target < 0) {
             throw new IllegalArgumentException("target不能小于0");
         }
         if (target <= 2) {
             return target;
         }
-        arr[target] = RectCover(target - 1) + RectCover(target - 2);
+        arr[target] = rectCover(target - 1) + rectCover(target - 2);
         return arr[target];
     }
 }
