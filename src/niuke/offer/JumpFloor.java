c5e3a34075a792ac0fa761861b27d71931778ce0
xjk971020 <823840954@qq.com>
2019-07-12 11:28:22 +0800

    跳台阶

--- src/niuke/offer/JumpFloor.java
+++ src/niuke/offer/JumpFloor.java
@@ -4,9 +4,19 @@ package niuke.offer;
  * author:xjk
  * 2019/6/13 15:23
  * niuke.offer
+ * 跳台阶
+ * 题目描述
  * 一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）。
  */
 public class JumpFloor {
+    private int[] arr = new int[1024];
+
+    public JumpFloor() {
+        for (int i = 0; i < arr.length; ++i) {
+            arr[i] = -1;
+        }
+    }
+
     public int JumpFloor(int target) {
         if (target < 0) {
             throw new IllegalArgumentException("目标数不能小于0");
@@ -17,6 +27,10 @@ public class JumpFloor {
         if (target <= 2) {
             return target;
         }
-        return JumpFloor(target - 1) + JumpFloor(target - 2);
+        if (arr[target] != -1) {
+            return arr[target];
+        }
+        arr[target] = JumpFloor(target - 1) + JumpFloor(target - 2);
+        return arr[target];
     }
 }
