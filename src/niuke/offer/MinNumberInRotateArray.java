97fac135726826c52044b24d4d13597210bad12c
xjk971020 <823840954@qq.com>
2019-07-12 10:54:43 +0800

    旋转数组的最小数字

--- src/niuke/offer/MinNumberInRotateArray.java
+++ src/niuke/offer/MinNumberInRotateArray.java
@@ -11,7 +11,45 @@ package niuke.offer;
  * NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。
  **/
 public class MinNumberInRotateArray {
-    public int minNumberInRotateArray(int [] array) {
+    /**
+     * 双指针法
+     * 一前一后两个指针进行遍历
+     * 时间复杂度高
+     * @param array
+     * @return
+     */
+    public int minNumberInRotateArray1(int [] array) {
+        if (array.length == 0 || array == null) {
+            return 0;
+        }
+        int pre = 0;
+        int next = 1;
+        while (next <= array.length) {
+            if (array[pre] > array[next]) {
+                return array[next];
+            }
+            pre++;
+            next++;
+        }
+        return 0;
+    }
+
+    /**
+     * 二分查找:
+     *   会有这样三种情况:
+     * (1)0 1 1 1 1 这样的数组旋转之后会变成 1 1 1 0 1 或者 1 0 1 1 1
+     *    这种情况二分查找不起作用, 只能在mid位置到末尾进行遍历找出最小值
+     * (2)array[mid] > array[high]:
+     *    出现这种情况的array类似[3,4,5,6,0,1,2]，此时最小数字一定在mid的右边。
+     *    low = mid + 1
+     * (3)array[mid] < array[high]:
+     *    出现这种情况的array类似[2,2,3,4,5,6,6],此时最小数字一定就是array[mid]或者在mid的左
+     *    边。因为右边必然都是递增的。
+     *    high = mid
+     * @param array
+     * @return
+     */
+    public int minNumberInRotateArray2(int [] array) {
         if (array.length == 0 || array == null) {
             return 0;
         }
@@ -23,10 +61,11 @@ public class MinNumberInRotateArray {
                 indexMid = index2;
                 break;
             }
-            indexMid = (index2 + index1)/2;
+            indexMid = ((index2 + index1)>>1);
             if (array[index1] == array[indexMid] && array[index2] == array[indexMid]) {
                 return minInOrder(array, index1, index2);
             }
+            //这个地方有点绕
             if (array[indexMid] >= array[index1]) {
                 index1 = indexMid;
             } else if (array[indexMid] <= array[index2]) {
@@ -35,10 +74,10 @@ public class MinNumberInRotateArray {
         }
         return array[indexMid];
     }
-    public  int minInOrder(int [] array, int index1, int index2) {
+
+    private  int minInOrder(int [] array, int index1, int index2) {
         for (;index1 < index2; ++index1) {
             if (array[index1] > array[index1+1]) {
-                System.out.println(index1);
                 return array[index1+1];
             }
         }
