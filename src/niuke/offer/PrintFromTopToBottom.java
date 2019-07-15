a57a036db85dc2beb81fab2be08a3d4dc2ce91d7
xjk971020 <823840954@qq.com>
2019-07-15 16:45:31 +0800

    从上往下打印二叉树

--- src/niuke/offer/PrintFromTopToBottom.java
+++ src/niuke/offer/PrintFromTopToBottom.java
@@ -4,13 +4,19 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 /**
-
  * author:xjk
  * 2019/6/14 15:32
  * niuke.offer
+ * 从上往下打印二叉树
+ * 题目描述
  * 从上往下打印出二叉树的每个节点，同层节点从左至右打印。
  */
 public class PrintFromTopToBottom {
+    /**
+     * 使用队列遍历每一层
+     * @param root
+     * @return
+     */
     public ArrayList<Integer> printFromTopToBottom(TreeNode root) {
         ArrayList<Integer> result = new ArrayList<>();
         if (root == null) {
