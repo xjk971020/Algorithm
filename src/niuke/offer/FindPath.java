2ab1a49e85d3e3f077eb9614f5fd7329c5882ccb
xjk971020 <823840954@qq.com>
2019-07-15 15:57:30 +0800

    二叉树中和为某一值的路径

--- src/niuke/offer/FindPath.java
+++ src/niuke/offer/FindPath.java
@@ -12,20 +12,34 @@ import java.util.ArrayList;
  * 路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。(注意: 在返回值的list中，数组长度大的数组靠前)
  */
 public class FindPath {
-  private ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-  private ArrayList<Integer> path = new ArrayList<>();
-  public ArrayList<ArrayList<Integer>> findPath(TreeNode root, int target) {
-     if (root == null) {
-         return result;
-     }
-     path.add(root.val);
-     target -= root.val;
-     if (target == 0 && root.left == null && root.right == null) {
-         result.add(new ArrayList<>(path));
-     }
-     findPath(root.left, target);
-     findPath(root.right, target);
-     path.remove(path.size() - 1);
-     return result;
-  }
+
+    private ArrayList<ArrayList<Integer>> result;
+    private ArrayList<Integer> path;
+
+    public FindPath() {
+        result = new ArrayList<>();
+        path = new ArrayList<>();
+    }
+
+    /**
+     * 根节点往下递归遍历每一条路的和
+     * @param root
+     * @param target
+     * @return
+     */
+    public ArrayList<ArrayList<Integer>> findPath(TreeNode root, int target) {
+        if (root == null) {
+            return result;
+        }
+        path.add(root.val);
+        target -= root.val;
+        if (target == 0 && root.left == null && root.right == null) {
+            //这里的path必须重新开辟内存, 不然会覆盖
+            result.add(new ArrayList<>(path));
+        }
+        findPath(root.left, target);
+        findPath(root.right, target);
+        path.remove(path.size() - 1);
+        return result;
+    }
 }
