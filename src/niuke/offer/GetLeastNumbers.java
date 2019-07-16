a9a5d5941686b64dfd84e3808863f113f5897028
xjk971020 <823840954@qq.com>
2019-07-16 12:04:35 +0800

    最小的K个数

--- src/niuke/offer/GetLeastNumbers.java
+++ src/niuke/offer/GetLeastNumbers.java
@@ -13,22 +13,29 @@ import java.util.PriorityQueue;
  * 例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
  **/
 public class GetLeastNumbers {
+    /**
+     * 构建一个大根堆, 将数据流中的数依次加到堆中, 此过程中保证每次大根堆中大的数是数据流中最小的几个数
+     * @param input
+     * @param k
+     * @return
+     */
     public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
         ArrayList<Integer> result = new ArrayList<>();
-        if (input == null || input.length == 0 || k == 0) {
+        //此处需要注意边界, k == 0 的时候
+        if (input == null || input.length == 0 || k <= 0) {
             return result;
         }
         if (k > input.length) {
             throw new RuntimeException("k不能大于input数组的长度");
         }
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1, o2) -> o2 - o1);
-        for (int i = 0; i < input.length; i++) {
+        for (int anInput : input) {
             if (maxHeap.size() != k) {
-                maxHeap.offer(input[i]);
-            } else if (maxHeap.peek() > input[i]) {
+                maxHeap.offer(anInput);
+            } else if (maxHeap.peek() > anInput) {
                 Integer temp = maxHeap.poll();
                 temp = null;
-                maxHeap.offer(input[i]);
+                maxHeap.offer(anInput);
             }
         }
         maxHeap.forEach(var -> {
