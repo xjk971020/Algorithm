6e6e98b9498ad8cf1cc16d72f6269cda904e6589
xjk971020 <823840954@qq.com>
2019-07-16 10:10:46 +0800

    复杂链表的复制

--- src/niuke/offer/Clone.java
+++ src/niuke/offer/Clone.java
@@ -13,7 +13,7 @@ package niuke.offer;
 public class Clone{
     public RandomListNode clone(RandomListNode pHead) {
         cloneNode(pHead);
-        cloneNode(pHead);
+        connectCloneNode(pHead);
         return reConnectNodes(pHead);
     }
     /**
@@ -63,6 +63,44 @@ public class Clone{
             node = node.next;
         }
         return pClonedHead;
+    }
+
+    /**
+     * @param pHead
+     * @return
+     */
+    public RandomListNode Clone(RandomListNode pHead) {
+        if (pHead == null) {
+            return null;
+        }
+
+        RandomListNode currentNode = pHead;
+        //1、复制每个结点，如复制结点A得到A1，将结点A1插到结点A后面；
+        while (currentNode != null) {
+            RandomListNode cloneNode = new RandomListNode(currentNode.label);
+            RandomListNode nextNode = currentNode.next;
+            currentNode.next = cloneNode;
+            cloneNode.next = nextNode;
+            currentNode = nextNode;
+        }
+
+        currentNode = pHead;
+        //2、重新遍历链表，复制老结点的随机指针给新结点，如A1.random = A.random.next;
+        while (currentNode != null) {
+            currentNode.next.random = currentNode.random == null? null : currentNode.random.next;
+            currentNode = currentNode.next.next;
+        }
+
+        currentNode = pHead;
+        RandomListNode pCloneHead = pHead.next;
+        //3、拆分链表，将链表拆分为原链表和复制后的链表
+        while (currentNode != null) {
+            RandomListNode cloneNode = currentNode.next;
+            currentNode.next = cloneNode.next;
+            cloneNode.next = cloneNode.next == null?null:cloneNode.next.next;
+            currentNode = currentNode.next;
+        }
+        return pCloneHead;
     }
 }
 
