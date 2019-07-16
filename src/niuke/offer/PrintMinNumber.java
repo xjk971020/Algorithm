72498069b4a027fb0e025921cb590743ac1dfed2
xjk971020 <823840954@qq.com>
2019-07-16 21:42:46 +0800

    把数组排成最小的数

--- src/niuke/offer/PrintMinNumber.java
+++ src/niuke/offer/PrintMinNumber.java
@@ -12,6 +12,17 @@ import java.util.Arrays;
  * 例如输入数组{3，32，321}，则打印出这三个数字能排成的最小数字为321323。
  */
 public class PrintMinNumber {
+    /**
+     * 定义比较器完成字符串的排序
+     * 先将整型数组转换成String数组，然后将String数组排序，最后将排好序的字符串数组拼接出来。关键就是制定排序规则。
+     * 排序规则如下：
+     * 若ab > ba 则 a < b，
+     * 若ab < ba 则 a > b，
+     * 若ab = ba 则 a = b；
+     * 比如 "3" < "31"但是 "331" > "313"，所以要将二者拼接起来进行比较
+     * @param numbers
+     * @return
+     */
     public String PrintMinNumber(int [] numbers) {
         if (numbers == null || numbers.length == 0) {
             return null;
