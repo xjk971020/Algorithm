9524d1e88a379d3758f80413f44861b4c68f732b
xjk971020 <823840954@qq.com>
2019-07-17 14:30:29 +0800

    和为S的连续正数序列

--- src/niuke/offer/FindContinuousSequence.java
+++ src/niuke/offer/FindContinuousSequence.java
@@ -13,6 +13,11 @@ import java.util.ArrayList;
  * 现在把问题交给你,你能不能也很快的找出所有和为S的连续正数序列? Good Luck!
  **/
 public class FindContinuousSequence {
+    /**
+     * 双指针技术，相当于有一个窗口，窗口的左右两边就是两个指针，我们根据窗口内值之和来确定窗口的位置和宽度。
+     * @param sum
+     * @return
+     */
     public ArrayList<ArrayList<Integer>> findContinuousSequence(int sum) {
         ArrayList<ArrayList<Integer>> res = new ArrayList<>();
         if (sum < 0) {
@@ -24,12 +29,12 @@ public class FindContinuousSequence {
         int small = 1;
         int big = 2;
         int curNum = big + small;
-        int middle = (1 + sum)/2;
-        while (small < middle) {
+        int middle = sum/2;
+        while (small <= middle) {
             if (curNum == sum) {
                 addContinuousSequence(res, small, big);
             }
-            while(small < middle && curNum > sum) {
+            while(small <= middle && curNum > sum) {
                 curNum -= small;
                 small++;
                 if (curNum == sum) {
