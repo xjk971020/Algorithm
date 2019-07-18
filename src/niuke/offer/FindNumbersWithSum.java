5f4a1a56b8de2e8f66212db216a4ecab940e4ed2
xjk971020 <823840954@qq.com>
2019-07-18 11:28:19 +0800

    和为S的两个数字

--- src/niuke/offer/FindNumbersWithSum.java
+++ src/niuke/offer/FindNumbersWithSum.java
@@ -5,12 +5,21 @@ import java.util.ArrayList;
 /**
  * @author xjk
  * @date 2019/5/17 -  23:27
+ * 和为S的两个数字
+ * 题目描述
  * 输入一个递增排序的数组和一个数字S，
  * 在数组中查找两个数，使得他们的和正好是S，
  * 如果有多对数字的和等于S，输出两个数的乘积最小的。
  **/
 public class FindNumbersWithSum {
-    public ArrayList<Integer> FindNumbersWithSum(int [] array, int sum) {
+    /**
+     * 思路:两个指针,一个从后往前,一个从前往后扫,将这两个位置的数相加看是否等于sum
+     * 利用数学规律,两个数和相等时, 差越大, 则乘积越小
+     * @param array
+     * @param sum
+     * @return
+     */
+    public ArrayList<Integer> findNumbersWithSum(int [] array, int sum) {
         ArrayList<Integer> list = new ArrayList<>();
         if (array == null || array.length < 2) {
             return list;
