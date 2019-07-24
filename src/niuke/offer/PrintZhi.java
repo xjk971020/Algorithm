b48fc69fb1650df434e6d436c18a95897bc40d1e
xjk971020 <823840954@qq.com>
2019-07-24 20:08:15 +0800

    按之字形顺序打印二叉树

--- src/niuke/offer/PrintZhi.java
+++ src/niuke/offer/PrintZhi.java
@@ -7,11 +7,13 @@ import java.util.Stack;
  * author:xjk
  * 2019/6/13 15:32
  * niuke.offer
+ * 按之字形顺序打印二叉树
+ * 题目描述
  * 请实现一个函数按照之字形打印二叉树，即第一行按照从左到右的顺序打印，
  * 第二层按照从右至左的顺序打印，第三行按照从左到右的顺序打印，其他行以此类推。
  */
 public class PrintZhi {
-    public ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+    public ArrayList<ArrayList<Integer>> print(TreeNode pRoot) {
         ArrayList<ArrayList<Integer>> result = new ArrayList<>();
         if (pRoot == null) {
             return result;
