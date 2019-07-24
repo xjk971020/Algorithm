a364cf65aad113fab43cb75bba9d3320a69722e7
xjk971020 <823840954@qq.com>
2019-07-24 23:03:56 +0800

    序列化二叉树

---
+++ src/niuke/offer/SerializeTree.java
@@ -0,0 +1,17 @@
+package niuke.offer;
+
+/**
+ * @Author:xjk
+ * @Date 2019/7/24 21:57
+ * 序列化二叉树
+ * 题目描述
+ * 请实现两个函数，分别用来序列化和反序列化二叉树
+ */
+public class SerializeTree {
+    public String Serialize(TreeNode root) {
+        return null;
+    }
+    public TreeNode Deserialize(String str) {
+        return null;
+    }
+}
