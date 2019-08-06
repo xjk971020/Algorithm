661a92276f45f892063eca304bbc3b57ed960a94
xjk971020 <823840954@qq.com>
2019-08-06 10:28:54 +0800

    正则表达式匹配

--- src/niuke/offer/Match.java
+++ src/niuke/offer/Match.java
@@ -37,7 +37,7 @@ public class Match {
         //str到尾，pattern未到尾(不一定匹配失败，因为a*可以匹配0个字符)
         if (str.length == strIndex && pattern.length != patternIndex) {
             //只有pattern剩下的部分类似a*b*c*的形式，才匹配成功
-            if (patternIndex + 1 < patternIndex && pattern[patternIndex+1] == '*') {
+            if (patternIndex + 1 < pattern.length && pattern[patternIndex+1] == '*') {
                 return matchCore(str, strIndex, pattern, patternIndex+2);
             }
             return false;
