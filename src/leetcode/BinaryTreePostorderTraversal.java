f859089fcd6477d3e6f37b810acd72c1e1088292
xjk971020 <823840954@qq.com>
2019-08-18 21:23:04 +0800

    binary-tree-postorder-traversal

---
+++ src/leetcode/BinaryTreePostorderTraversal.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import java.util.ArrayList;
+
+/**
+ * cathetine
+ * 19-8-18 下午9:06
+ * binary-tree-postorder-traversal
+ * 题目描述
+ * 求给定的二叉树的后序遍历。
+ * 例如：
+ * 给定的二叉树为{1,#,2,3},
+ *     1↵    ↵     2↵    /↵   3↵
+ * 返回[3,2,1].
+ * 备注；用递归来解这道题太没有新意了，可以给出迭代的解法么？
+ *
+ * Given a binary tree, return the postorder traversal of its nodes' values.
+ * For example:
+ * Given binary tree{1,#,2,3},
+ *    1↵    ↵     2↵    /↵   3↵
+ *
+ * return[3,2,1].
+ *
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ */
+public class BinaryTreePostorderTraversal {
+    private ArrayList<Integer> list = new ArrayList<>();
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.left == null && root.right == null) {
+            list.add(root.val);
+        }
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        list.add(root.val);
+        return list;
+    }
+}
