e7cd3459f20bb298cbe965b0a78ff2b6c8acd0f4
xjk971020 <823840954@qq.com>
2019-08-23 10:54:35 +0800

    word-break

---
+++ src/leetcode/WordBreak.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import java.util.Set;
+
+/**
+ * @author:xjk 2019/8/23 9:24
+ * leetcode
+ * word-break
+ * 题目描述
+ * 给定一个字符串s和一组单词dict，判断s是否可以用空格分割成一个单词序列，使得单词序列中所有的单词都是dict中的单词（序列可以包含一个或多个单词）。
+ * 例如:
+ * 给定s=“leetcode”；
+ * dict=["leet", "code"].
+ * 返回true，因为"leetcode"可以被分割成"leet code".
+ *
+ * Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+ * For example, given
+ * s ="leetcode",
+ * dict =["leet", "code"].
+ *
+ * Return true because"leetcode"can be segmented as"leet code".
+ */
+public class WordBreak {
+    public boolean wordBreak(String s, Set<String> dict) {
+        if (s == null || s.length() == 0) {
+            return false;
+        }
+        boolean[] flag = new boolean[s.length() + 1];
+        flag[0] = true;
+        for (int i = 1; i <= s.length(); ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (flag[j] && dict.contains(s.substring(j,i))) {
+                    flag[i] = true;
+                    break;
+                }
+            }
+        }
+        return flag[s.length()];
+    }
+}
