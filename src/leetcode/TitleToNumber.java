4b792417d95a86cbc81de9d45a28c2e9759ef422
xjk971020 <823840954@qq.com>
2019-08-27 17:07:07 +0800

    titleToNumber

---
+++ src/leetcode/TitleToNumber.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+/**
+ * @Author:cathetine
+ * @Date:19-8-27 下午5:01
+ * 171. Excel表列序号
+ * 给定一个Excel表格中的列名称，返回其相应的列序号。
+ *
+ * 例如，
+ *
+ *     A -> 1
+ *     B -> 2
+ *     C -> 3
+ *     ...
+ *     Z -> 26
+ *     AA -> 27
+ *     AB -> 28
+ *     ...
+ * 示例 1:
+ *
+ * 输入: "A"
+ * 输出: 1
+ * 示例 2:
+ *
+ * 输入: "AB"
+ * 输出: 28
+ * 示例 3:
+ *
+ * 输入: "ZY"
+ * 输出: 701
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/excel-sheet-column-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class TitleToNumber {
+    public int titleToNumber(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            count = count * 26 + s.charAt(i) - 'A' + 1;
+        }
+        return count;
+    }
+}
