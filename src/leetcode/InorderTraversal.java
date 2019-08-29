ee9f788db0a3f8fefdf38ed9c5b2357092c9e257
xjk971020 <823840954@qq.com>
2019-08-29 12:54:17 +0800

    94. 二叉树的中序遍历

---
+++ src/leetcode/InorderTraversal.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author: xjk
+ * @Date: 2019/8/29
+ * @Time: 12:52
+ * 94. 二叉树的中序遍历
+ * 给定一个二叉树，返回它的中序 遍历。
+ *
+ * 示例:
+ *
+ * 输入: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * 输出: [1,3,2]
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-inorder-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。c
+ */
+public class InorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> results = new ArrayList<>();
+        if (root == null) {
+            return results;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        while (!stack.isEmpty() || root != null) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                root = stack.pop();
+                results.add(root.val);
+                root = root.right;
+            }
+        }
+        return results;
+    }
+}
