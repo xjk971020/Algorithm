4af94d9361164073e1921459ae040521a4bc25c4
xjk971020 <823840954@qq.com>
2019-08-29 11:35:03 +0800

    144. 二叉树的前序遍历

---
+++ src/leetcode/PreorderTraversal.java
@@ -0,0 +1,55 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author: xjk
+ * @Date: 2019/8/29
+ * @Time: 11:34
+ * 144. 二叉树的前序遍历
+ * 给定一个二叉树，返回它的 前序 遍历。
+ *
+ *  示例:
+ *
+ * 输入: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * 输出: [1,2,3]
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-preorder-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class PreorderTraversal {
+    /**
+     * 非递归
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> results = new ArrayList<>();
+        if (root == null) {
+            return results;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            root = stack.pop();
+            results.add(root.val);
+            if (root.right != null) {
+                stack.push(root.right);
+            }
+            if (root.left != null) {
+                stack.push(root.left);
+            }
+        }
+        return results;
+    }
+}
