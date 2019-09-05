6f126f09d929b71688de2914ccb466bc6c1d0eb8
xjk971020 <823840954@qq.com>
2019-09-05 16:07:29 +0800

    5. 最长回文子串

---
+++ src/leetcode/LongestPalindrome.java
@@ -0,0 +1,54 @@
+package leetcode;
+
+/**
+ * @Author:cathetine
+ * @Date:19-9-5
+ * @Time:下午3:24
+ * 5. 最长回文子串
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 示例 2：
+ * <p>
+ * 输入: "cbbd"
+ * 输出: "bb"
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-palindromic-substring
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LongestPalindrome {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() <= 1) {
+            return s;
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            int len = process(s, i, i);
+            int len1 = process(s, i, i + 1);
+            int length = Math.max(len, len1);
+            if (length > end - start) {
+                start = i - (length - 1) / 2;
+                end = i + length / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int process(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+    public static void main(String[] args) {
+        LongestPalindrome longestPalindrome = new LongestPalindrome();
+        System.out.println(longestPalindrome.longestPalindrome("123432,1"));
+    }
+}
