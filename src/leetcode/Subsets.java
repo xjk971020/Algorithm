909dafdf6088428098f8c620d8d80453f0177428
xjk971020 <823840954@qq.com>
2019-09-21 12:31:26 +0800

    78. 子集

---
+++ src/leetcode/Subsets.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author:cathetine
+ * @Date:19-9-21
+ * @Time:上午11:01
+ * 78. 子集
+ * 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ * <p>
+ * 说明：解集不能包含重复的子集。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: nums = [1,2,3]
+ * 输出:
+ * [
+ * [3],
+ *   [1],
+ *   [2],
+ *   [1,2,3],
+ *   [1,3],
+ *   [2,3],
+ *   [1,2],
+ *   []
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/subsets
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        result.add(new ArrayList<>());
+        for (int i = 0; i < nums.length; i++) {
+            int length = result.size();
+            for (int j = 0; j < length; j++) {
+                List<Integer> temp = new ArrayList<>(result.get(j));
+                temp.add(nums[i]);
+                result.add(temp);
+            }
+        }
+        return result;
+    }
+
+}
