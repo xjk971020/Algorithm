cc6383ff1cd7dfa5b55a98872b8f853d651c2ec4
xjk971020 <823840954@qq.com>
2019-09-27 21:17:09 +0800

    重建二叉树

--- src/niuke/offer/ReConstructBinaryTree.java
+++ src/niuke/offer/ReConstructBinaryTree.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
  * author:xjk
  * 2019/6/18 11:42
  * niuke.offer
+ * 重建二叉树
  * 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。
  * 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
  * 例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
@@ -20,6 +21,7 @@ public class ReConstructBinaryTree {
             return null;
         }
         TreeNode root = new TreeNode(pre[0]);
+        //此处的i必须从０开始,因为头节点可能都在第一个位置
         for (int i = 0; i < in.length; ++i) {
             if (root.val == in[i]) {
                 root.left = reConstructBinaryTree(Arrays.copyOfRange(pre, 1, i+1),Arrays.copyOfRange(in, 0, i));
