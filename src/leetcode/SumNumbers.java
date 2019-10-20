80454dd81ab45af3510eeeac4ecb7a00dd1c4ff4
xjk971020 <823840954@qq.com>
2019-10-20 13:33:57 +0800

    129. 求根到叶子节点数字之和

---
+++ src/leetcode/SumNumbers.java
@@ -0,0 +1,56 @@
+package leetcode;
+
+/**
+ * @Author:cathetine
+ * @Date:19-10-20
+ * @Time:下午12:23
+ * 129. 求根到叶子节点数字之和
+ * <p>
+ * 给定一个二叉树，它的每个结点都存放一个 0-9 的数字，每条从根到叶子节点的路径都代表一个数字。
+ * <p>
+ * 例如，从根到叶子节点路径 1->2->3 代表数字 123。
+ * <p>
+ * 计算从根到叶子节点生成的所有数字之和。
+ * <p>
+ * 说明: 叶子节点是指没有子节点的节点。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,2,3]
+ * 1
+ * / \
+ * 2   3
+ * 输出: 25
+ * 解释:
+ * 从根到叶子节点路径 1->2 代表数字 12.
+ * 从根到叶子节点路径 1->3 代表数字 13.
+ * 因此，数字总和 = 12 + 13 = 25.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sum-root-to-leaf-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class SumNumbers {
+    int total = 0;
+
+    public int sumNumbers(TreeNode root) {
+        if (root == null) {
+            return total;
+        }
+        getAllPathTotal(0, root);
+        return total;
+    }
+
+    private void getAllPathTotal(int sum, TreeNode root) {
+        if (root.left == null && root.right == null) {
+            total = total + sum + root.val;
+            return;
+        }
+        if (root.left != null) {
+            getAllPathTotal(sum * 10 + root.val * 10, root.left);
+        }
+        if (root.right != null) {
+            getAllPathTotal(sum * 10 + root.val * 10, root.right);
+        }
+    }
+}
