cd308afb829328dd1fe4c045430ad54cc945f206
xjk971020 <823840954@qq.com>
2019-10-30 11:18:10 +0800

    16. 最接近的三数之和

---
+++ src/leetcode/_16_ThreeSumClosest.java
@@ -0,0 +1,55 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author: xjk
+ * @Date: 2019/10/30
+ * @Time: 11:04
+ * 16. 最接近的三数之和
+ * <p>
+ * 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+ * <p>
+ * 例如，给定数组 nums = [-1，2，1，-4], 和 target = 1.
+ * <p>
+ * 与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/3sum-closest
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class _16_ThreeSumClosest {
+    /**
+     * 双指针法，遍历一遍数组，在每一次遍历的过程中利用当前数的后一个数作为头指针，
+     * 数组中最后一个数作为尾指针进行双指针遍历知道找到最小的数
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int threeSumClosest(int[] nums, int target) {
+        if (nums == null || nums.length < 3) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int result = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length - 2; ++i) {
+            int start = i + 1;
+            int end = nums.length - 1;
+            while (start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+                if (Math.abs(result - target) > Math.abs(sum - target)) {
+                    result = sum;
+                }
+                if (sum > target) {
+                    end--;
+                } else if (sum < target) {
+                    start++;
+                } else if (sum == target) {
+                    return 0;
+                }
+            }
+        }
+        return result;
+    }
+}
