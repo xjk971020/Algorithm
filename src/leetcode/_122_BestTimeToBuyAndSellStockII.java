08e5bc93c5c5130175ee9a68835640b47d03e3cb
xjk971020 <823840954@qq.com>
2019-12-10 21:24:26 +0800

    122. 买卖股票的最佳时机 II

--- src/leetcode/_122_BestTimeToBuyAndSellStockII.java
+++ src/leetcode/_122_BestTimeToBuyAndSellStockII.java
@@ -3,7 +3,7 @@ package leetcode;
 /**
  * @Author:cathetine
  * @Date:19-8-25 下午11:20
- * best-time-to-buy-and-sell-stock-ii
+ * 122. 买卖股票的最佳时机 II
  * 题目描述
  * 假设你有一个数组，其中第i个元素表示某只股票在第i天的价格。
  * 设计一个算法来寻找最大的利润。你可以完成任意数量的交易(例如，多次购买和出售股票的一股)。但是，你不能同时进行多个交易(即，你必须在再次购买之前卖出之前买的股票)。
@@ -16,7 +16,7 @@ package leetcode;
  * However, you may not engage in multiple transactions at the same time
  * (ie, you must sell the stock before you buy again).
  */
-public class BestTimeToBuyAndSellStockII {
+public class _122_BestTimeToBuyAndSellStockII {
     public int maxProfit(int[] prices) {
         if (prices == null || prices.length == 0) {
             return 0;
