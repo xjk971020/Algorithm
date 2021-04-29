1984f797ed7a7dd52b8a8a799497ac6aad56dac4
xjk971020 <823840954@qq.com>
2021-04-29 22:29:18 +0800

    85. 最大矩形 496. 下一个更大元素 I

---
+++ src/leetcode/_496_NextGreaterElement.java
@@ -0,0 +1,65 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @Author:xjk
+ * @Description:
+ *
+ * 496. 下一个更大元素 I
+ *
+ * 给你两个 没有重复元素 的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。
+ *
+ * 请你找出 nums1 中每个元素在 nums2 中的下一个比其大的值。
+ *
+ * nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。如果不存在，对应位置输出 -1 。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: nums1 = [4,1,2], nums2 = [1,3,4,2].
+ * 输出: [-1,3,-1]
+ * 解释:
+ *     对于 num1 中的数字 4 ，你无法在第二个数组中找到下一个更大的数字，因此输出 -1 。
+ *     对于 num1 中的数字 1 ，第二个数组中数字1右边的下一个较大数字是 3 。
+ *     对于 num1 中的数字 2 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
+ * 示例 2:
+ *
+ * 输入: nums1 = [2,4], nums2 = [1,2,3,4].
+ * 输出: [3,-1]
+ * 解释:
+ *     对于 num1 中的数字 2 ，第二个数组中的下一个较大数字是 3 。
+ *     对于 num1 中的数字 4 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/next-greater-element-i
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Date: 2021/4/29
+ */
+public class _496_NextGreaterElement {
+
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            throw new IllegalArgumentException();
+        }
+
+        int[] result = new int[nums1.length];
+
+        int nums1Length = nums1.length;
+        int nums2Length = nums2.length;
+
+        for (int i = 0; i < nums1.length; i++) {
+            if (i >= nums2Length) {
+                Arrays.fill(result, i, nums1.length, -1);
+                break;
+            }
+
+
+        }
+
+        return result;
+    }
+
+}
